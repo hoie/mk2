29456cec34c1dbe5d65d5027500031caa0a1eae3
Oyvind Hoie <hoie@draft.no>
2015-08-05 20:07:07 +0200

    Implemented Sliding sidebar menu

--- scripts/main.js
+++ scripts/main.js
@@ -10,19 +10,41 @@ $('document').ready (function() {
     
     new WOW().init();
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeOutExpo');
-        event.preventDefault();
+
+   
+
+    // Sliding sidebar menu
+    
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
     });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
     });
+
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    $("#sidebar-wrapper").toggleClass("active");
+                    return false;
+                }
+            }
+        });
+    });
+
 
     // if ($(window).width() > '1024') {
     //     $(window).stellar();
